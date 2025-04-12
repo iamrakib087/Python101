4571d3fcddbf585c7b12164306cf1d02a2523add
Md.Abdullah Al Rakib <122741559+iamrakib087@users.noreply.github.com>
2025-04-12 23:02:21 +0600

    Create 6_5_number_problem.py

---
+++ 6_5_number_problem.py
@@ -0,0 +1,7 @@
+rivers = {
+    'nile': 'egypt',
+    'jamuna': 'bangladesh',
+    'amazon': 'brazil'
+}
+for river, country in rivers.items():
+    print(f"The {river.title()} runs through {country.title()}.") 
