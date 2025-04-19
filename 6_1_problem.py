0cd6eba504f0259011f50105beedc32d6330a8b7
Md.Abdullah Al Rakib <iamrakib087@gmail.com>
2025-04-19 16:59:40 +0600

    .

---
+++ 6_1_problem.py
@@ -0,0 +1,9 @@
+person = {
+    'first_name': 'Md.',
+    'last_name': 'Abdullah Al Rakib',
+    'age': 25,
+    'city': 'Dhaka'
+}
+
+for key, value in person.items():
+    print(f"{key.capitalize()}: {value}") 
