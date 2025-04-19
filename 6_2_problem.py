f328355c16f54822d4c6e73a94f80a9d7bf16bad
Md.Abdullah Al Rakib <iamrakib087@gmail.com>
2025-04-19 17:04:35 +0600

    .

---
+++ 6_2_problem.py
@@ -0,0 +1,11 @@
+fav_numbers = {
+    'Rakib': 12,
+    'Ayon': 333,
+    'Ruhi': 14,
+    'Plabon': 7,
+    'Emon': 9
+}
+
+# Printing each person's name and their favorite number
+for name, number in fav_numbers.items():
+    print(f"{name}'s favorite number is {number}.") 
