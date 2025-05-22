4b9b020c623f42045f46b6a312bcc285d46353f5
Md.Abdullah Al Rakib <iamrakib087@gmail.com>
2025-05-22 23:08:16 +0600

    .

---
+++ Chapter_8/task_8_6.py
@@ -0,0 +1,6 @@
+def city_country(city, country):
+    return f"{city}, {country}"
+
+print(city_country("Santiago", "Chile"))
+print(city_country("Kobe", "Japan"))
+print(city_country("Dhaka", "Bangladesh"))
