9f8d253ef851e47deda8d79eec1aa74c577ad53c
Md.Abdullah Al Rakib <iamrakib087@gmail.com>
2025-05-22 22:51:58 +0600

    .

---
+++ Chapter_8/task_8_3.py
@@ -0,0 +1,6 @@
+def make_shirt(size, message):
+    print(f"Shirt size is {size} and it will have the message: '{message}' printed on it.")
+
+make_shirt("Large", "Born to be creative, forced to code")
+
+make_shirt(message="Last minute engineer", size="Medium")
