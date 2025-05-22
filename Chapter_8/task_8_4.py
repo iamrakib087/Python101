b8f639ec6f8138a436414a5310d4e9f247e3143c
Md.Abdullah Al Rakib <iamrakib087@gmail.com>
2025-05-22 22:55:19 +0600

    .

---
+++ Chapter_8/task_8_4.py
@@ -0,0 +1,8 @@
+def make_shirt(size="Large", message="I love Python"):
+    print(f"Shirt size is {size} and it will have the message: '{message}' printed on it.")
+
+make_shirt()
+
+make_shirt(size="Medium")
+
+make_shirt(size="Small", message="Debug mode: ON")
